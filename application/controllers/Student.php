d9090fcfca35ff387d6c0bc92b468b99ecd18164
Gurpreet Singh <gurpreet2501@gmail.com>
2018-07-04 22:18:26 +0530

    code changed for adding student id

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -26,20 +26,23 @@ class Student extends CI_Controller
 	{ 
 
 			$crud = new grocery_CRUD();
-			$crud->columns('id','profile_image','email','phone','city','registration_confirmation');
+			$crud->columns('student_unique_code','email','phone','city','registration_date','registration_confirmation');
 			$crud->set_table('students_registration');
 			// $crud->set_relation('user_id','users','username');
 			$crud->where('added_by',user_id());
 			$crud->display_as('id','#Student Id');
+			$crud->callback_after_insert(array($this, 'update_student_unique_code'));
+			$crud->callback_after_update(array($this, 'update_student_unique_code'));
 			$crud->set_relation_n_n('courses', 'students_registration_courses', 'courses', 'students_registration_id', 'course_id', 'course_name','',['courses.added_by' => user_id()]);
 			$crud->unset_texteditor('address');
 			$crud->field_type('registration_confirmation', 'dropdown', array('1' => 'Confirmed', '0' => 'Pending'));
-			$crud->unique_fields(array('email','phone'));
+			// $crud->unique_fields(array('email','phone'));
 			$crud->required_fields('name','email','phone','city','address','highest_qualification');
 			$crud->set_field_upload('profile_image','assets/uploads/students/profile_images');
 			if($crud->getState()=='add')
 				$crud->field_type('registration_confirmation','hidden',0);
 			$crud->field_type('added_by','hidden',user_id());
+			$crud->field_type('student_unique_code','hidden');
 			$crud->field_type('registration_date','hidden',date('Y-m-d H:i:s'));
 			$crud->field_type('created_at','hidden',date('Y-m-d H:i:s'));
 			$crud->field_type('updated_at','hidden',date('Y-m-d H:i:s'));
@@ -48,6 +51,19 @@ class Student extends CI_Controller
 			$this->_example_output($output);
 
 	} 
+
+  function update_student_unique_code($post_array,$primary_key)
+	{
+		
+		$data = array(
+			"student_unique_code" => 'STU'.$primary_key,
+		);
+
+		$this->db->update('students_registration', $data, array('id' => $primary_key));	 
+			 
+		return true;
+	}
+
 		public function courses()
 	{ 
 
