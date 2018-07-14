afe7f2c65c5dab220c0261aaa624c5d4187f3f90
Gurpreet Singh <gurpreet2501@gmail.com>
2018-07-14 21:23:30 +0530

    code added

--- application/views/fees/payment_selection.php
+++ application/views/fees/payment_selection.php
@@ -5,13 +5,13 @@
 		<h3>FEES PAYMENT MODULE</h3>
 		<form action="<?=site_url('fees/payment_post')?>" method="post">
 			<div class="form-group">
-				<select class="form-control" name="student_id">
+				<select class="form-control chosen-select" name="student_id">
 					<option selected="true" disabled="true" >-Select Student-</option>		
 					<?php foreach ($students as $key => $student): ?>
 									<option value="<?=$student->id?>"><?=$student->student_unique_code?></option>
 					<?php endforeach ?>			
 				</select>
-				<br>
+				<div class="margin-top-10"></div>
 				<input type="submit" name="" class="btn btn-success full-width" value="Pay Fees" />
 			</div>
 		</form>
