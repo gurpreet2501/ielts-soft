a7f995cac93bc33a4452dda1a3b1e8bdbe3e29be
Gurpreet Singh <gurpreet2501@gmail.com>
2018-09-10 08:36:15 +0530

    jquery datepicker date range modified

--- assets/grocery_crud/js/jquery_plugins/config/jquery.datepicker.config.js
+++ assets/grocery_crud/js/jquery_plugins/config/jquery.datepicker.config.js
@@ -1,11 +1,12 @@
 $(function(){
 	$('.datepicker-input').datepicker({
-			dateFormat: js_date_format,
+			dateFormat: js_date_format, 
 			showButtonPanel: true,
 			changeMonth: true,
-			changeYear: true
+			changeYear: true,
+			yearRange: '1950:'+(new Date()).getFullYear() 
 	});
-	
+	 
 	$('.datepicker-input-clear').button();
 	
 	$('.datepicker-input-clear').click(function(){
